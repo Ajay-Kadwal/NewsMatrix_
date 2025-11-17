3d977b22c84e4727c7180469133ef1f6a37a5c10
Ajay Kadwal <ajaykadwal.work@gmail.com>
2025-11-17 11:09:26 +0530

    Initial Commit

---
+++ NewsMatrix/NewsMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMatrixApp.swift
+//  NewsMatrix
+//
+//  Created by AJAY KADWAL on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
